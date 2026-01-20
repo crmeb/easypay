10db95592d143b9bf55b13efde6a0fa42306c19e
evoxwht <442384644@qq.com>
2026-01-20 16:37:45 +0800

    docs(Enum): 修正注释格式并添加新的支付相关URL常量
    统一注释格式，删除多余空格，并新增二维码支付退款、订单关闭和订单退款的URL常量

--- src/Enum/PayUnionMerEnum.php
+++ src/Enum/PayUnionMerEnum.php
@@ -4,31 +4,34 @@ namespace Crmeb\Easypay\Enum;
 
 class PayUnionMerEnum
 {
-    //  获取token
+    // 获取token
     const TOKEN_API_URL = '/v1/token/access';
-    //   获取二维码
+    // 获取二维码
     const NATIVE_PAY_URL = '/v1/netpay/bills/get-qrcode';
-    //   支付宝H5支付
+    // 二维码支付退款
+    const NATIVE_PAY_REFUND_URL = '/v1/netpay/bills/refund';
+    // 支付宝H5支付
     const ALIPAY_H5_PAY_URL = '/v1/netpay/trade/h5-pay';
-    //  微信H5支付
-    const WECHAT_H5_PAY_URL = '/v1/netpay/wxpay/h5-pay';
-
-    //   支付宝APP支付
+    // 支付宝APP支付
     const ALIPAY_APP_PAY_URL = '/v1/netpay/trade/precreate';
-    //   微信APP支付
+    // 微信H5支付
+    const WECHAT_H5_PAY_URL = '/v1/netpay/wxpay/h5-pay';
+    // 微信APP支付
     const WECHAT_APP_PAY_URL = '/v1/netpay/wx/unified-order';
-
-    //   微信小程序支付
+    // 微信小程序支付
     const MINI_PAY_URL = '/v1/netpay/wx/unified-order';
-
-    //  微信JSAPI支付
+    // 微信JSAPI支付
     const WECHAT_JSAPI_PAY_URL = '/v1/netpay/wx/unified-order';
+    // 订单关闭
+    const CLOSE_ORDER_URL = '/v1/netpay/close';
+    // 订单退款
+    const REFUND_ORDER_URL = '/v1/netpay/refund';
 
-    //  支付类型
+    // 支付类型
     const UNION_TYPE_ALIPAY = 'alipay';
     const UNION_TYPE_WECHAT = 'wechat';
 
-    //  网关映射
+    // 网关映射
     const GATEWAY_MAP = [
         PayGatewayTypeEnum::APP_PAY    => [
             self::UNION_TYPE_ALIPAY => self::ALIPAY_APP_PAY_URL,
