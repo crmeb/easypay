7a69078c1b9ff1676df30b81c79a03fb7eeeaaf8
evoxwht <442384644@qq.com>
2026-01-22 17:03:58 +0800

    feat(Gateway): 添加小程序支付和公众号支付功能并优化支付参数
    - 新增miniPay方法支持小程序支付
    - 新增jsapiPay方法支持公众号/JSAPI支付
    - 优化h5Pay方法参数结构，使用merOrderId替代billNo
    - 添加refund和close方法支持退款和关闭订单

--- src/Gateway/UnionMerService.php
+++ src/Gateway/UnionMerService.php
@@ -58,7 +58,7 @@ class UnionMerService
      * @throws GuzzleException
      * @throws InvalidArgumentException
      */
-    public function scan(string $orderId, string $amount, string $subject, string $bodyDesc = '', array $goods = [], array $subOrders = [])
+    public function scan(string $orderId, string $amount, string $subject = '', string $bodyDesc = '', array $goods = [], array $subOrders = [])
     {
         $params = [
             'union_type'  => PayUnionMerEnum::UNION_TYPE_WECHAT,
@@ -91,17 +91,16 @@ class UnionMerService
      * @throws PayException
      * @throws PayResponseException
      */
-    public function h5Pay(string $orderId, string $amount, string $subject, string $bodyDesc = '', array $goods = [], array $subOrders = [], string $unionType = PayUnionMerEnum::UNION_TYPE_WECHAT)
+    public function h5Pay(string $orderId, string $amount, string $subject = '', string $bodyDesc = '', array $goods = [], array $subOrders = [], string $unionType = PayUnionMerEnum::UNION_TYPE_WECHAT)
     {
         $params = [
             'union_type'  => $unionType,
             'instMid'     => 'H5DEFAULT',
+            'merOrderId'  => $orderId,
             'msgId'       => Tools::guid(),
             'srcReserve'  => $subject,
-            'billNo'      => $orderId,
             'totalAmount' => bcmul($amount, 100, 0),
-            'billDesc'    => $bodyDesc,
-            'billDate'    => date('Y-m-d'),
+            'orderDesc'   => $bodyDesc,
         ];
 
         if ($goods) {
@@ -112,5 +111,118 @@ class UnionMerService
         }
 
         return $this->payGateway->pay(PayGatewayTypeEnum::WAP_PAY, $params);
+    }
+
+    /**
+     * 小程序支付
+     * @param string $orderId 订单号
+     * @param string $amount 支付金额
+     * @param string $appid 小程序appid
+     * @param string $openid 小程序openid
+     * @param string $subject 标记原样返回
+     * @param string $bodyDesc 商品描述
+     * @param array $goods 商品列表
+     * @param array $subOrders 子订单列表
+     * @return array
+     * @throws PayException
+     * @throws PayResponseException
+     * @throws GuzzleException
+     * @throws InvalidArgumentException
+     */
+    public function miniPay(string $orderId, string $amount, string $appid, string $openid, string $subject = '', string $bodyDesc = '', array $goods = [], array $subOrders = [])
+    {
+        $params = [
+            'union_type'  => PayUnionMerEnum::UNION_TYPE_WECHAT,
+            'instMid'     => 'MINIDEFAULT',
+            'msgId'       => Tools::guid(),
+            'srcReserve'  => $subject,
+            'merOrderId'  => $orderId,
+            'tradeType'   => 'MINI',
+            'subAppId'    => $appid,
+            'subOpenId'   => $openid,
+            'totalAmount' => bcmul($amount, 100, 0),
+            'orderDesc'   => $bodyDesc,
+        ];
+
+        if ($goods) {
+            $params['goods'] = $goods;
+        }
+        if ($subOrders) {
+            $params['subOrders'] = $subOrders;
+        }
+
+        return $this->payGateway->pay(PayGatewayTypeEnum::MINI_PAY, $params);
+    }
+
+    /**
+     * 公众号/JSAPI 支付
+     * @param string $orderId 订单号
+     * @param string $amount 支付金额（元）
+     * @param string $appid 公众号或商户 appid
+     * @param string $openid 用户 openid
+     * @param string $subject 附加数据，原样返回
+     * @param string $bodyDesc 商品描述
+     * @param array $goods 商品明细列表
+     * @param array $subOrders 子订单列表
+     * @return array 支付响应结果
+     * @throws PayException
+     * @throws PayResponseException
+     * @throws GuzzleException
+     * @throws InvalidArgumentException
+     */
+    public function jsapiPay(string $orderId, string $amount, string $appid, string $openid, string $subject = '', string $bodyDesc = '', array $goods = [], array $subOrders = [])
+    {
+        $params = [
+            'union_type'  => PayUnionMerEnum::UNION_TYPE_WECHAT,
+            'instMid'     => 'YUEDANDEFAULT',
+            'msgId'       => Tools::guid(),
+            'srcReserve'  => $subject,
+            'merOrderId'  => $orderId,
+            'subAppId'    => $appid,
+            'subOpenId'   => $openid,
+            'totalAmount' => bcmul($amount, 100, 0),
+            'orderDesc'   => $bodyDesc,
+        ];
+
+        if ($goods) {
+            $params['goods'] = $goods;
+        }
+        if ($subOrders) {
+            $params['subOrders'] = $subOrders;
+        }
+
+        return $this->payGateway->pay(PayGatewayTypeEnum::JSAPI_PAY, $params);
+    }
+
+    /**
+     * 退款
+     * @param array $order 退款订单参数
+     * @param string $type 支付类型 包含 qrcode、h5、app、mini、jsapi
+     * @return array 退款结果
+     * @throws PayException
+     * @throws PayResponseException
+     * @throws GuzzleException
+     * @throws InvalidArgumentException
+     */
+    public function refund(array $order = [], string $type)
+    {
+        $order['refundAmount'] = bcmul($order['refundAmount'], 100, 0);
+        return $this->payGateway->refund($order, $type);
+    }
+
+    /**
+     * 关闭/撤销订单
+     * @param string $orderId 商户订单号
+     * @param string $type 支付类型 包含 qrcode、h5、app、mini、jsapi
+     * @param string $srcReserve 附加数据，原样返回
+     * @return array 关闭结果
+     * @throws PayException
+     * @throws PayResponseException
+     * @throws GuzzleException
+     * @throws InvalidArgumentException
+     */
+    public function close(string $orderId, string $type, string $srcReserve = '')
+    {
+        return $this->payGateway->close($orderId, $type, $srcReserve);
     }
 }
